7d0726a84e50c5390b625ea6aea8190457785515
MKKrylov <mkkrylov@inno.tech>
2022-11-22 18:38:40 +0300

    test

--- src/main/java/ru/maaax/elasticsearchservice/ElasticSearchServiceApplication.java
+++ src/main/java/ru/maaax/elasticsearchservice/ElasticSearchServiceApplication.java
@@ -1,13 +1,23 @@
 package ru.maaax.elasticsearchservice;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ElasticSearchServiceApplication {
+public class ElasticSearchServiceApplication implements CommandLineRunner {
+    @Value("${spring.elasticsearch.uris}")
+    private String elasticUrl;
 
     public static void main(String[] args) {
         SpringApplication.run(ElasticSearchServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("1111111111111111111111111111");
+        System.out.println(elasticUrl);
     }
 
 }
